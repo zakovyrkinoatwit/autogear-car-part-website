69e349755ff4a213537ffcb35598e2281681e01b
Oleg Zakovyrkin <zakovyrkino@wit.edu>
2024-04-02 18:57:55 -0400

    Adjusted login window
    Added signup/register js functionality.

--- functionality.js
+++ functionality.js
@@ -1,3 +1,4 @@
+/* Hero Slides */
 let slideIndex = 0;
 showSlides();
 
@@ -16,4 +17,37 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+/* User Button */
+document.addEventListener('DOMContentLoaded', (event) => {
+  const userIcon = document.getElementById('user-icon');
+  const loginForm = document.getElementById('login-form');
+  const login = document.getElementById('login');
+  const register = document.getElementById('register');
+
+  userIcon.addEventListener('click', function(e) {
+      loginForm.style.display = loginForm.style.display === 'block' ? 'none' : 'block';
+      e.stopPropagation();
+  });
+
+  document.addEventListener('click', function(e) {
+      if (!loginForm.contains(e.target) && e.target !== userIcon) {
+          loginForm.style.display = 'none';
+      }
+  });
+
+  loginForm.addEventListener('click', function(e) {
+      e.stopPropagation();
+  });
+});
+
+function showRegister() {
+  document.getElementById('login').style.display = 'none';
+  document.getElementById('register').style.display = 'block';
+}
+
+function showLogin() {
+  document.getElementById('login').style.display = 'block';
+  document.getElementById('register').style.display = 'none';
 }
