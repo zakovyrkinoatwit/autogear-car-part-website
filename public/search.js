64a5d23a1544c81682003292efd6dc5e414334b8
Enrique Esteve <estevee@wit.edu>
2024-04-03 22:48:13 -0400

    Adding to cart functions

--- public/search.js
+++ public/search.js
@@ -1,102 +1,119 @@
 let products = {
     data: [
         {
+            id: "1",
             productName: "Metallic Brake Pads",
             category: "Parts",
             price: "90",
             image: "part-photos/car-metallicbrakes.png"
         },
         {
+            id: "2",
             productName: "Organic Brake Pads",
             category: "Parts",
             price: "60",
             image: "part-photos/car-organicbrakes.png"
         },
         {
+            id: "3",
             productName: "Ceramic Brake Pads",
             category: "Parts",
             price: "30",
             image: "part-photos/car-ceramicbrakes.png"
         },
         {
+            id: "4",
             productName: "Performance Wheel",
             category: "Wheels and Tires",
             price: "150",
             image: "part-photos/car-wheel.png"
         },
         {
+            id: "5",
             productName: "All-Season Tire",
             category: "Wheels and Tires",
             price: "100",
             image: "part-photos/car-wheel.png"
         },
         {
+            id: "6",
             productName: "Body Kit",
             category: "Exterior Parts",
             price: "2300",
             image: "part-photos/car-exterior.png"
         },
         {
+            id: "7",
             productName: "Tow Hitch",
             category: "Exterior Parts",
             price: "120",
             image: "part-photos/car-towhitch.png"
         },
         {
+            id: "8",
             productName: "Custom Bumper",
             category: "Body Parts",
             price: "230",
             image: "part-photos/car-bumper.png"
         },
         {
+            id: "9",
             productName: "Front Fender",
             category: "Body Parts",
             price: "120",
             image: "part-photos/car-fender.png"
         },
         {
+            id: "10",
             productName: "Inner Fender",
             category: "Body Parts",
             price: "100",
             image: "part-photos/car-innerfender.png"
         },
         {
+            id: "11",
             productName: "Headlight Bulb",
             category: "Lighting",
             price: "10",
             image: "part-photos/car-lightbulb.png"
         },
         {
+            id: "12",
             productName: "Signal",
             category: "Lighting",
             price: "20",
             image: "part-photos/car-signallight.png"
         },
         {
+            id: "13",
             productName: "Floor Mat",
             category: "Interior",
             price: "400",
             image: "part-photos/car-mat.png"
         },
         {
+            id: "14",
             productName: "Seat",
             category: "Interior",
             price: "400",
             image: "part-photos/car-seatcover.png"
         },
         {
+            id: "15",
             productName: "Shift Knob",
             category: "Interior",
             price: "45",
             image: "part-photos/car-shiftknob.png"
         },
         {
+            id: "16",
             productName: "Pedals",
             category: "Interior",
             price: "155",
             image: "part-photos/car-pedals.png"
         },
         {
+            id: "16",
             productName: "Wheel Cover",
             category: "Interior",
             price: "25",
